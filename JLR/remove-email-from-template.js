3906304034c25b3958bf7117ac0508fbcfb5c447
Sara <sarkhalid77@gmail.com>
2026-06-26 05:27:55 +0300

    Remove email from employee import template

---
+++ JLR/remove-email-from-template.js
@@ -0,0 +1,44 @@
+﻿const fs = require('fs');
+
+let s = fs.readFileSync('app/admin/employees/page.js', 'utf8');
+
+// Remove email from visible template/help text
+s = s.replaceAll(
+  'Columns: Full Name, Email, Employee ID, ID Number, Department',
+  'Columns: Full Name, Employee ID, ID Number, Department'
+);
+
+s = s.replaceAll(
+  'Columns: Full Name, Email Address, Employee ID, ID Number, Department',
+  'Columns: Full Name, Employee ID, ID Number, Department'
+);
+
+// Remove Email from common template header arrays
+s = s.replaceAll(
+  "['Full Name', 'Email', 'Employee ID', 'ID Number', 'Department']",
+  "['Full Name', 'Employee ID', 'ID Number', 'Department']"
+);
+
+s = s.replaceAll(
+  '["Full Name", "Email", "Employee ID", "ID Number", "Department"]',
+  '["Full Name", "Employee ID", "ID Number", "Department"]'
+);
+
+s = s.replaceAll(
+  "['Full Name', 'Email Address', 'Employee ID', 'ID Number', 'Department']",
+  "['Full Name', 'Employee ID', 'ID Number', 'Department']"
+);
+
+s = s.replaceAll(
+  '["Full Name", "Email Address", "Employee ID", "ID Number", "Department"]',
+  '["Full Name", "Employee ID", "ID Number", "Department"]'
+);
+
+// Remove email fields from sample rows if present
+s = s.replaceAll(/,\s*Email:\s*['"][^'"]*['"]/g, '');
+s = s.replaceAll(/,\s*'Email Address':\s*['"][^'"]*['"]/g, '');
+s = s.replaceAll(/,\s*"Email Address":\s*['"][^'"]*['"]/g, '');
+
+fs.writeFileSync('app/admin/employees/page.js', s, 'utf8');
+
+console.log('Email removed from employee template.');
