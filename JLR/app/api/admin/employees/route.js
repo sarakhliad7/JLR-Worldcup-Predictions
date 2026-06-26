d970ce262305158448ef31a95ddb24b3107c29ae
Sara <sarkhalid77@gmail.com>
2026-06-26 06:31:53 +0300

    Remove employee email completely

--- JLR/app/api/admin/employees/route.js
+++ JLR/app/api/admin/employees/route.js
@@ -1,6 +1,6 @@
 ﻿import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
-import { hashPassword } from '@/lib/auth';
+import { prisma } from '../../../../lib/prisma';
+import { hashPassword } from '../../../../lib/auth';
 
 export const dynamic = 'force-dynamic';
 
